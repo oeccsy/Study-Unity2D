d2e1ab3075ce3455ba30652a449a6ac9d5d03074
oeccsy <oeccsy@gmail.com>
2021-06-22 01:20:37 +0900

    대화시스템,말풍선 구현.md Update

--- SampleApp/App1/Assets/Script/GameManager.cs
+++ SampleApp/App1/Assets/Script/GameManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,7 +8,8 @@ public class GameManager : MonoBehaviour
 {
     public TalkManager talkManager;
     public Text talkText;
-    public int talkIndex;
+    public int talkIndex = 0;
+    public Image portraitImg;
 
     public GameObject scanObject;
     public bool isAction =false ;
@@ -16,15 +18,13 @@ public class GameManager : MonoBehaviour
 
     public void Action(GameObject scanObj)
     {
-
         scanObject = scanObj;
         ObjectData objData = scanObject.GetComponent<ObjectData>();
-        Talk(objData.id, objData.isNpc);        //id, isnpc 정보로 text에 적절한 값 저장
+        Talk(objData.id);                       //id 정보로 text에 적절한 값 저장
         talkPanel.SetActive(isAction);          //isAction값에 따라 말풍선 On/Off
-
     }
-
-    void Talk(int id, bool isNpc)
+    
+    void Talk(int id)
     {
         string talkData = talkManager.GetTalk(id, talkIndex);
 
@@ -35,15 +35,9 @@ public class GameManager : MonoBehaviour
             return;
         }
 
-        if(isNpc)
-        {
-            talkText.text = talkData;
-        }
-        else
-        {
-            talkText.text = talkData;
-
-        }
+        talkText.text = talkData.Split(':')[0];     //talkData를 :를 기준으로 구분한 문자열 배열의 0번째 Index를 가져온다.
+        Debug.Log(Convert.ToInt32(talkData.Split(':')[1]));
+        portraitImg.sprite = talkManager.GetPortrait(id, Convert.ToInt32(talkData.Split(':')[1]));
 
         isAction = true;
         talkIndex++;
--- SampleApp/App1/Assets/Script/TalkManager.cs
+++ SampleApp/App1/Assets/Script/TalkManager.cs
@@ -5,20 +5,32 @@ using UnityEngine;
 public class TalkManager : MonoBehaviour
 {
     Dictionary<int, string[]> talkData;
+    Dictionary<int, Sprite> portraitData;
+
+    public Sprite[] portraitArr;
 
     void Awake()
     {
         talkData = new Dictionary<int, string[]>();
+        portraitData = new Dictionary<int, Sprite>();
         GenerateData();
     }
 
-    void GenerateData()
+    void GenerateData() //"text:portraitIndex"
     {
-        talkData.Add(0, new string[] { "안녕?", "안녕안녕 안녕??" });
-        talkData.Add(1, new string[] { "안녕?", "안녕안녕 안녕??" });
-        talkData.Add(2, new string[] { "좋은 날씨다." });
-        talkData.Add(3, new string[] { "안좋은 날씨다." });
-        talkData.Add(4, new string[] { "안녕?", "안녕안녕 안녕??" });
+        talkData.Add(10, new string[] { "안녕?:0", "안녕안녕 안녕??:1" });
+        talkData.Add(20, new string[] { "안녕?:0", "안녕안녕 안녕??:1" });
+        talkData.Add(30, new string[] { "좋은 날씨다.:0" });
+        talkData.Add(40, new string[] { "안좋은 날씨다.:0" });
+        talkData.Add(50, new string[] { "그저 그런 날씨다.:0" });
+
+        portraitData.Add(10+0, portraitArr[0]);
+        portraitData.Add(10+1, portraitArr[1]);
+        portraitData.Add(20+0, portraitArr[0]);
+        portraitData.Add(20+1, portraitArr[1]);
+        portraitData.Add(30, portraitArr[2]);
+        portraitData.Add(40, portraitArr[3]);
+        portraitData.Add(50, portraitArr[4]);
     }
 
     public string GetTalk(int id, int talkIndex)
@@ -27,5 +39,10 @@ public class TalkManager : MonoBehaviour
             return null;
 
         return talkData[id][talkIndex];
+    }
+
+    public Sprite GetPortrait(int id, int portraitIndex)
+    {
+        return portraitData[id + portraitIndex];
     }
 }
